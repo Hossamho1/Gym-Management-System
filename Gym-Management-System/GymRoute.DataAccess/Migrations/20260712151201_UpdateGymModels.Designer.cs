c235e719f90e61b705bd74b48785fac917a4d35e
Hossam <hossammostafa203203@gmail.com>
2026-07-13 02:43:05 +0300

    local migrations

--- Gym-Management-System/GymRoute.DataAccess/Migrations/20260712151201_UpdateGymModels.Designer.cs
+++ Gym-Management-System/GymRoute.DataAccess/Migrations/20260712151201_UpdateGymModels.Designer.cs
@@ -4,6 +4,7 @@ using GymRoute.DataAccess.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GymRoute.DataAccess.Migrations
 {
     [DbContext(typeof(GymDbContext))]
-    partial class GymDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260712151201_UpdateGymModels")]
+    partial class UpdateGymModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Gym-Management-System/GymRoute.DataAccess/Migrations/20260712151201_UpdateGymModels.cs
+++ Gym-Management-System/GymRoute.DataAccess/Migrations/20260712151201_UpdateGymModels.cs
@@ -5,13 +5,13 @@
 namespace GymRoute.DataAccess.Migrations
 {
     /// <inheritdoc />
-    public partial class updateTables : Migration
+    public partial class UpdateGymModels : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropIndex(
-                name: "IX_Users_Email_phone",
+                name: "IX_Users_Email_Phone",
                 table: "Users");
 
             migrationBuilder.DropCheckConstraint(
@@ -19,57 +19,14 @@ namespace GymRoute.DataAccess.Migrations
                 table: "Users");
 
             migrationBuilder.RenameColumn(
-                name: "phone",
+                name: "Photo",
                 table: "Users",
-                newName: "Phone");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Email",
-                table: "Users",
-                type: "nvarchar(100)",
-                maxLength: 100,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(200)",
-                oldMaxLength: 200);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Users_Email_Phone",
-                table: "Users",
-                columns: new[] { "Email", "Phone" },
-                unique: true);
-
-            migrationBuilder.AddCheckConstraint(
-                name: "CK_GymUser_Phone",
-                table: "Users",
-                sql: "LEN(Phone) = 11 AND (Phone LIKE '010%' OR Phone LIKE '011%' OR Phone LIKE '012%' OR Phone LIKE '015%')");
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_Users_Email_Phone",
-                table: "Users");
-
-            migrationBuilder.DropCheckConstraint(
-                name: "CK_GymUser_Phone",
-                table: "Users");
+                newName: "photo");
 
             migrationBuilder.RenameColumn(
                 name: "Phone",
                 table: "Users",
                 newName: "phone");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Email",
-                table: "Users",
-                type: "nvarchar(200)",
-                maxLength: 200,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(100)",
-                oldMaxLength: 100);
 
             migrationBuilder.CreateIndex(
                 name: "IX_Users_Email_phone",
@@ -81,6 +38,39 @@ namespace GymRoute.DataAccess.Migrations
                 name: "CK_GymUser_Phone",
                 table: "Users",
                 sql: "LEN(phone) = 11 AND (phone LIKE '010%' OR phone LIKE '011%' OR phone LIKE '012%' OR phone LIKE '015%')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email_phone",
+                table: "Users");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_GymUser_Phone",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "photo",
+                table: "Users",
+                newName: "Photo");
+
+            migrationBuilder.RenameColumn(
+                name: "phone",
+                table: "Users",
+                newName: "Phone");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email_Phone",
+                table: "Users",
+                columns: new[] { "Email", "Phone" },
+                unique: true);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_GymUser_Phone",
+                table: "Users",
+                sql: "LEN(Phone) = 11 AND (Phone LIKE '010%' OR Phone LIKE '011%' OR Phone LIKE '012%' OR Phone LIKE '015%')");
         }
     }
 }
